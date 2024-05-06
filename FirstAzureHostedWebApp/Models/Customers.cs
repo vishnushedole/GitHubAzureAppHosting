f2ea15d2bcc88a860cde2d260a36dee5e72d5fba
vishnu shedole <vishnushedole09@gmail.com>
2024-05-06 09:48:32 +0530

    First commit

---
+++ FirstAzureHostedWebApp/Models/Customers.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstAzureHostedWebApp.Models
+{
+    public class Customer
+    {
+        [Key]
+        public string CustomerId { get; set; }
+        public string CompanyName { get; set; } 
+        public string ContactName {  get; set; }
+        public string City { get; set; }
+
+        public string Country {  get; set; }
+    }
+    public class CustomerDbContext:DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public CustomerDbContext(DbContextOptions<CustomerDbContext> options) : base(options) 
+        {
+
+        }
+    }
+}
---
+++ FirstAzureHostedWebApp/Program.cs
@@ -0,0 +1,31 @@
+using FirstAzureHostedWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+var connstr = builder.Configuration.GetConnectionString("CustomerConnection");
+
+builder.Services.AddDbContext<CustomerDbContext>(options =>
+{
+    options.UseSqlServer(connstr);
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
